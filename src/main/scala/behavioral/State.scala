355800452c1206c48d0d8ebefa65a7e9fbb991d2
Daniel James <dwhjames@gmail.com>
2012-05-21 12:33:23 +0100

    expand State pattern demo
    introduce a DSL for building finite state machines (again from http://groovy.codehaus.org/State+Pattern)

--- src/main/scala/behavioral/State.scala
+++ src/main/scala/behavioral/State.scala
@@ -9,6 +9,10 @@ object StateDemo {
     c.connect()
     c send "baz"
     c.disconnect()
+    
+    println()
+    
+    dsl.DSLDemo.demo()
   }
 }
 
@@ -51,5 +55,93 @@ object Connection {
     override def send(c: Connection, s: String) = c.internalSend(s)
     override def disconnect(c: Connection) = changeState(c, Offline)
     override def toString() = "Online"
+  }
+}
+
+package dsl {
+  object DSLDemo {
+    def demo() {
+      val fsm = new FiniteStateMachine[String,String]("offline")
+      fsm.record.onEvent("connect").inState("offline").moveToState("online")
+      fsm.record.onEvent("send").stayInState("online")
+      fsm.record.inState("online").onEvent("disconnect").moveToState("offline")
+      
+      fsm.fire("send")
+      fsm.fire("connect")
+      fsm.fire("send")
+      fsm.fire("connect")
+      fsm.fire("disconnect")
+    }
+  }
+
+  class TransitionBuilder[S,E](private val fsm: FiniteStateMachine[S,E]) {
+    private[dsl] var fromState: S = _
+    private[dsl] var transEvent: E = _
+    private[dsl] var toState: S = _
+
+    def inState(state: S) = {
+      fromState = state
+      this
+    }
+
+    def onEvent(event: E) = {
+      transEvent = event
+      this
+    }
+
+    def moveToState(state: S) {
+      toState = state
+      fsm register this
+    }
+
+    def stayInState(state: S) {
+      fromState = state
+      toState = state
+      fsm register this
+    }
+  }
+
+  class FiniteStateMachine[S,E](val initialState: S) {
+    import scala.collection.mutable.HashMap
+    val machine: HashMap[S,HashMap[E,S]] = new HashMap
+    var currentState = initialState
+
+    def reset() {
+      currentState = initialState
+    }
+
+    def record = new TransitionBuilder(this)
+
+    private def stateError(s: S) {
+      println("no transitions defined for state: " + s)
+    }
+
+    private def transError(e: E) {
+      println("no '" + e + "' transition defined for state: " + currentState)
+    }
+
+    def fire(event: E) {
+      machine.get(currentState) match {
+        case None =>
+          stateError(currentState)
+        case Some(map) =>
+          map.get(event) match {
+            case None =>
+              transError(event)
+            case Some(state) =>
+              println(event + ": " + currentState + " -> " + state)
+              currentState = state
+          }
+      }
+    }
+
+    private[dsl] def register(b: TransitionBuilder[S,E]) {
+      machine.get(b.fromState) match {
+        case None =>
+          machine(b.fromState) = HashMap(b.transEvent -> b.toState)
+        case Some(map) =>
+          map += (b.transEvent -> b.toState)
+      }
+    }
   }
 }
